0ea81e45990350c5dc2cb1a18f11c41f9f5d1d3d
Viviann Rutan <rutanvt@dukes.jmu.edu>
2017-03-22 00:54:55 -0400

    Name, Date, Time, Change Made

--- public/Main.php
+++ public/Main.php
@@ -12,7 +12,7 @@ mysqli_select_db($con, "wildlifeDB");
     <!-- Developed by David Speroni, on my honor. -->
     <head>
         <meta charset="utf-8">
-        <title>Contractor System by David Speroni</title>
+        <title>Contractor System by Viviann Rutan</title>
         <meta name="description" content="">
         <link rel="stylesheet" href="conStyles.css">
         <h1>Contractor System by David Speroni</h1>
